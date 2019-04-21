be892f457e961b2fab2f766f6337a35a5c0ddb57
Lu-Yung Huang <royleon4@gmail.com>
2019-04-22 02:58:09 +1200

    ver1... may have issues rerouting and late response but its bascally done

--- rip_v2.py
+++ rip_v2.py
@@ -4,8 +4,8 @@ Routing Information Protocol
 (RIPv2)
 '''
 
-import os, sys, re, time, random
-
+import sys, re, time, random
+import threading
 import socket, select
 import pickle, struct
 
@@ -16,16 +16,17 @@ output_ports = {}
 router_id, loop = 0, 0
 table = {}
 listen_list = []
-
+neighbors = []
 
 
 #CONSTANTS
 ##TIMERS_FOR_RIPv2 = [30, 180, 120]
-UPDATE = 30 / 6
-TIMEOUT = 180 / 6
-GARBAGE = (120 + 30) / 6
+UPDATE = 30 / 30
+TIMEOUT = (180) / 30 + UPDATE
+GARBAGE = (120) / 30 + TIMEOUT
 INFINITY = 16
 HOST = "127.0.0.1"
+
 
 
 def domain_check(value, mini, maxi):
@@ -95,6 +96,7 @@ def syntax_check(config_data):
             print("Port:", oport, "was specified in input ports!")
             exit(1)
         output_ports[oport] = [metric, dest]
+        neighbors.append(dest)
         
         
     #print(local_table["router-id"])
@@ -191,18 +193,18 @@ def init_table():
         dest = item[1]
         via = dest
         metric = item[0]
-        table[dest] = [via, metric, 0.00, "data loaded"]
+        table[dest] = [via, metric, 0.00, "directly connected"]
 
 def show_table():
     #print("|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|")
-    print("ROUTER_ID:", router_id)
+    print("{:^49}".format("ROUTER_ID: " + str(router_id)))
     print("\nLoop:", loop)
     
     print("| ID   | via |metric| time  | message            ")
     print(" ------------------------------------------------>")
-    for key, value in table.items():
+    for key, value in sorted(table.items()):
         print("|{:^6}|{:^5}|{:^6}|{:^7.3f}|{:^20}".format(key, value[0], value[1], value[2], value[3]))
-    print("|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|")
+    print("|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\/|\n")
 
 
 def init_router():
@@ -223,51 +225,120 @@ def is_valid_pack(packet):
         return True
     return False
 
+def trigger_update():
+    send_message()
+    
+
 def update_table(pack):
     from_id = struct.unpack("H", pack["id"])[0]
     #print(from_id)
-    for dest, new_metric in pack["data"].items():
-        if new_metric <= 16:
-            if dest in table:
+    #updating the neighbor first
+    if from_id in table:
+        table[from_id][3] = "regular update"
+        table[from_id][2] = 0         
+        
+    else:
+        details = [from_id, pack["metric"], 0, "new neighbor"]
+        table[from_id] = details
+        neighbors.append(from_id)
+             
+        
+    for dest, distance in pack["data"].items():
+        #print("in update table:", dest)
+        #print("table:", table)
+        
+        #print("dest:", dest)
+        #print("tb:", table)
+        new_metric = min(distance + table[from_id][1], INFINITY)
+        if dest in table:
+            if new_metric < INFINITY and dest not in neighbors:   
                 metric = table[dest][1]
-                #via = table[dest][0]
-                table[dest][1] = min(metric, new_metric+table[from_id][1])
-                if new_metric > metric:
+                if new_metric < metric:
+                    table[dest][1] = min(new_metric, INFINITY)
                     table[dest][0] = from_id
                     table[dest][3] = "metric updated"
-                table[dest][3] = "regular update"
-                
+                    table[dest][2] = 0
+                elif table[dest][0] == from_id:
+                    table[dest][3] = "regular update"
+                    table[dest][2] = 0
+                        
             else:
-                details = [from_id, new_metric + table[from_id][1], 0, "newly created"]
-                table[dest] = details
-                
-            table[dest][2] = 0
+                if table[dest][0] == from_id:
+                    table[dest][2] = max(table[dest][2], TIMEOUT)
+                    if table[dest][1] != INFINITY:
+                        table[dest][1] == INFINITY
+                        send_message()
+                        time.sleep(0.2)  
+        else:
+            if new_metric < INFINITY:
+                #details = [from_id, new_metric, 0, "newly created"]
+                table[dest] = [from_id, new_metric, 0, "newly created"]
+            
+        
     pass
+
+def refresh_table():
+    to_remove = []
+    start = time.time()
+    time.sleep(random.uniform(0.8, 0.2))
+    output_ids = []
+       
+    for key in table.keys():
+        elapsed = time.time() - start 
+        table[key][2] += elapsed
+        time_recorded = table[key][2]
+        if time_recorded >= GARBAGE:
+            to_remove.append(key)
+        elif time_recorded > UPDATE and time_recorded < TIMEOUT:
+            table[key][3] = "Possibly lost"
+            
+        elif time_recorded >= TIMEOUT:
+            table[key][3] = "Invalid"
+            table[key][1] = INFINITY
+            
+    
+    for key in to_remove:
+        table.pop(key)
+        if key in neighbors:
+            neighbors.remove(key)
+            
+        #output_ports.pop(key)
+        
+
 
 def recieve_msg(timeout):
     readable,write,error = select.select(listen_list,[],[], timeout)
+    print("received packs:\n")
     for recieved in readable:
         #recieved.settimeout(10)
         data, addr = recieved.recvfrom(1024)
+        #print(data)
         pack = pickle.loads(data)
         #print("received:", pack)
         if is_valid_pack(pack):
+            
+            print(pack)
             update_table(pack)
+    print("=================================")
         
         
     pass
 
 
-def create_pack(dest_id):
+def create_pack(dest_id, port):
     pack = {}
+    #print("TABLE:", table)
     pack["ver"] = 2
     pack["id"] = struct.pack("H", router_id)
+    pack["metric"] = output_ports[port][0]
+    #pack["from_id"] = router_id
     #print("dfdgs", struct.unpack("H",  pack["id"]))
     pack["data"] = {}
     for dest, item in table.items():
         via = item[0]
         metric = item[1]
-        if not via == dest_id and dest != dest_id:
+        if (not via == dest_id and dest != dest_id) or\
+           metric == INFINITY:
             pack["data"][dest] = item[1]
     return pack
 
@@ -279,23 +350,30 @@ def send_message():
     
     for port in output_ports.keys():
         send_to = output_ports[port][1]
-        pack = create_pack(send_to)
-        packet = pickle.dumps(pack)
+        pack = create_pack(send_to, port)
+        packet = pickle.dumps(pack, protocol=2)
         sock.sendto(packet, (HOST, port))
 
 def exchange_info():
+    def regular_update():
+        while 1:
+            interval = random.uniform(UPDATE*0.8, UPDATE*0.2)
+            time.sleep(interval)
+            send_message()
+        
+        
+    regu_upda = threading.Thread(target = regular_update)
+    regu_upda.start()
+    
     global loop
     while 1:
-        start = time.time()
-        to_update = random.uniform(UPDATE*0.8, UPDATE*0.2)
-        time_passed = time.time() - start
-        
-        while time_passed < to_update:
-            recieve_msg(time_passed)
-            time_passed = time.time() - start
+        #time.sleep(2)
         loop += 1
+        
+        refresh_table()
         show_table()
-        send_message()    
+        #
+        recieve_msg(UPDATE) #timeout is 5 seconds
         
         
 def main_program(file_name):
